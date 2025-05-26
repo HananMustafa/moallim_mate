ba7d8e3e3bd5a36fdd1430f99a339c1aab61c19d
Hanan Mustafa <151427566+HananMustafa@users.noreply.github.com>
2025-05-26 16:37:55 +0500

    Setting project with physical android device

--- lib/res/endpoints.dart
+++ lib/res/endpoints.dart
@@ -1,6 +1,7 @@
 class endpoint {
   // static var baseUrl = 'http://127.0.0.1:8000/api/';
-  static var baseUrl = 'http://10.0.2.2:8000/api/'; //for android emulator
+  // static var baseUrl = 'http://10.0.2.2:8000/api/'; //for android emulator
+  static var baseUrl = 'http://172.17.241.0:8000/api/'; //for physical device
 
   static var loginEndpoint = '${baseUrl}login';
   static var getEventsEndpoint = '${baseUrl}getEvents';
