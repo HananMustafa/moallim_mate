7da7b9f6641b8e021ecfea89ee4c1478fbff6385
Hanan Mustafa <151427566+HananMustafa@users.noreply.github.com>
2025-06-20 11:24:06 +0500

    Used precacheImage to render the logo faster on splash screen

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -11,25 +11,68 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
+  bool _initialized = false;
 
-    Timer(const Duration(seconds: 1), () {
-      Navigator.pushNamed(context, RoutesName.dashboard);
-    });
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    if (!_initialized) {
+      _initialized = true;
+
+      // Preload image
+      precacheImage(const AssetImage('assets/icon/icon.png'), context).then((
+        _,
+      ) {
+        // Delay navigation after preload completes
+        Timer(const Duration(seconds: 1), () {
+          Navigator.pushNamed(context, RoutesName.dashboard);
+        });
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.primary,
-      body: Center(
-        child: Image.asset(
-          'assets/icon/icon.png',
-          width: 150, // Adjust size as needed
-        ),
-      ),
+      body: Center(child: Image.asset('assets/icon/icon.png', width: 150)),
     );
   }
 }
+
+// import 'dart:async';
+// import 'package:flutter/material.dart';
+// import 'package:moallim_mate/utils/routes/routes_name.dart';
+// import 'package:moallim_mate/res/color.dart';
+
+// class SplashScreen extends StatefulWidget {
+//   const SplashScreen({super.key});
+
+//   @override
+//   State<SplashScreen> createState() => _SplashScreenState();
+// }
+
+// class _SplashScreenState extends State<SplashScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+
+//     Timer(const Duration(seconds: 1), () {
+//       Navigator.pushNamed(context, RoutesName.dashboard);
+//     });
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: AppColors.primary,
+//       body: Center(
+//         child: Image.asset(
+//           'assets/icon/icon.png',
+//           width: 150, // Adjust size as needed
+//         ),
+//       ),
+//     );
+//   }
+// }
