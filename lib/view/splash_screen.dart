cdefcce3e23ace7c45b2977e08853c45a827dcb0
Hanan Mustafa <151427566+HananMustafa@users.noreply.github.com>
2025-06-24 15:01:14 +0500

    Displaying version number in splash screen

--- lib/view/splash_screen.dart
+++ lib/view/splash_screen.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:moallim_mate/utils/routes/routes_name.dart';
 import 'package:moallim_mate/res/color.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -12,6 +13,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   bool _initialized = false;
+  String _appVersion = '';
 
   @override
   void didChangeDependencies() {
@@ -24,6 +26,9 @@ class _SplashScreenState extends State<SplashScreen> {
       precacheImage(const AssetImage('assets/icon/icon.png'), context).then((
         _,
       ) {
+        // Fetch app version
+        _loadVersionInfo();
+
         // Delay navigation after preload completes
         Timer(const Duration(seconds: 1), () {
           Navigator.pushNamed(context, RoutesName.dashboard);
@@ -32,11 +37,43 @@ class _SplashScreenState extends State<SplashScreen> {
     }
   }
 
+  Future<void> _loadVersionInfo() async {
+    final info = await PackageInfo.fromPlatform();
+    setState(() {
+      _appVersion = 'Version ${info.version}';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    // return Scaffold(
+    //   backgroundColor: AppColors.primary,
+    //   body: Center(child: Image.asset('assets/icon/icon.png', width: 150)),
+    // );
+
     return Scaffold(
       backgroundColor: AppColors.primary,
-      body: Center(child: Image.asset('assets/icon/icon.png', width: 150)),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const SizedBox(), // Push content to center
+            Center(child: Image.asset('assets/icon/icon.png', width: 150)),
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                _appVersion,
+                style: const TextStyle(
+                  color: Colors.white70,
+                  fontSize: 12,
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
