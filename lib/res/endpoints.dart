690011ace0a06b2e439301791b7c857accf49366
Hanan Mustafa <151427566+HananMustafa@users.noreply.github.com>
2025-06-24 11:15:08 +0500

    fix

--- lib/res/endpoints.dart
+++ lib/res/endpoints.dart
@@ -1,7 +1,7 @@
 class endpoint {
   // static var baseUrl = 'http://127.0.0.1:8000/api/';
   // static var baseUrl = 'http://10.0.2.2:8000/api/'; //for android emulator
-  static var baseUrl = 'http://172.17.242.9:8000/api/'; //for physical device
+  static var baseUrl = 'http://172.17.241.29:8000/api/'; //for physical device
 
   static var loginEndpoint = '${baseUrl}login';
   static var getEventsEndpoint = '${baseUrl}getEvents';
