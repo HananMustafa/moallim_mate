1675a096e31a5cd5d187bf9ebdda124a9326f190
Hanan Mustafa <151427566+HananMustafa@users.noreply.github.com>
2025-06-30 11:41:33 +0500

    backend of render.com is connected

--- lib/res/endpoints.dart
+++ lib/res/endpoints.dart
@@ -1,7 +1,9 @@
 class endpoint {
   // static var baseUrl = 'http://127.0.0.1:8000/api/';
   // static var baseUrl = 'http://10.0.2.2:8000/api/'; //for android emulator
-  static var baseUrl = 'http://172.17.241.29:8000/api/'; //for physical device
+  // static var baseUrl = 'http://172.17.241.29:8000/api/'; //for physical device
+  static var baseUrl =
+      'https://laravel-backend-rq3p.onrender.com/api/'; //backend on render
 
   static var loginEndpoint = '${baseUrl}login';
   static var getEventsEndpoint = '${baseUrl}getEvents';
